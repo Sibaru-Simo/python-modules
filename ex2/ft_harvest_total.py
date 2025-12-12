1fb9ac2402391aec37a4d24d21d6c7838dfe4805
Sibaru-simo <s9ymosimo@gmail.com>
2025-12-12 12:24:26 +0100

    .

---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,8 @@
+def ft_harvest_total():
+	print("Day 1 harvest: ")
+	Day1 = int(input(""))
+	print("Day 2 harvest: ")
+	Day2 = int(input(""))
+	print("Day 3 harvest: ")
+	Day3 = int(input(""))
+	print("Total harvest: ", Day1 + Day2 + Day3)
